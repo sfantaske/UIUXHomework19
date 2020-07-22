cd8f81dbaf3488292b85586734674bc3f5b13fb0
sfantaske <sfantaske@gmail.com>
2020-07-22 17:52:51 -0600

    Got a simple modal to work

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,10 @@
-    console.log("JS init");
+console.log("JS init");
 
 
-$(".button").mouseenter(function(){
-alert("help");
+$("#btn1").on("click", function() {
+    $(".modal").css("display", "block");
+});
+
+$(".modal").on("click", function() {
+    $(".modal").css("display", "none");
 });
