22d0dfa267dc2545efdd29dad1381cafa131f351
sfantaske <sfantaske@gmail.com>
2020-07-22 18:47:24 -0600

    Updated modal

--- JS/index.js
+++ JS/index.js
@@ -2,9 +2,11 @@ console.log("JS init worked");
 
 
 $("#btn1").on("click", function() {
-    $(".modal").css("display", "block");
+    $(".modal").css("display", "inline");
+    $(".content").css("display", "none");
 });
 
 $(".modal").on("click", function() {
     $(".modal").css("display", "none");
+    $(".content").css("display", "block");
 });
